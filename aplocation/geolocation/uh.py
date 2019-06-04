7f8c9354631b86f313780072f0acac90439d37c8
Leonard <leonard@healthqtech.com>
2019-06-04 11:53:24 +0200

    some figuring out of requests

---
+++ aplocation/geolocation/uh.py
@@ -0,0 +1,29 @@
+import requests
+
+url = 'https://www.googleapis.com/geolocation/v1/geolocate'
+params = {
+    'key': '',
+}
+
+
+# TODO figure out if there is more information that can be used here
+payload = {
+    'wifiAccessPoints': [
+        {
+            'macAddress': '9c:b2:b2:66:c1:be',
+        },
+        {
+            'macAddress': '84:78:ac:b9:76:19',
+        },
+    ],
+}
+import json
+r = requests.post(
+    url=url, 
+    params=params,
+    json=json.dumps(payload)
+)
+
+
+print(r.url)
+print(r.text)
